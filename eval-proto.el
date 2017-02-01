2e92381c2caef87e5343e306468e14d7b9896f1c
Hershal Bhave <hershal.bhave@gmail.com>
2017-02-01 01:23:19 -0600

    super experimental yo

--- eval-proto.el
+++ eval-proto.el
@@ -4,6 +4,7 @@
 
 ;; Author: Hershal Bhave <hershal.bhave@gmail.com>
 ;; Keywords: convenience, extensions, tools
+;; Version: 0.0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
